5a194afac2e858454110b432e854c501b42f3113
Titouan Chevrier <chevrier.titouan@gmail.com>
2023-05-24 02:02:40 +0200

    rompich

--- Module00/ex01/phoneBook.hpp
+++ Module00/ex01/phoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: titouan_ck <titouan_ck@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 15:40:19 by tchevrie          #+#    #+#             */
-/*   Updated: 2023/05/20 16:00:05 by titouan_ck       ###   ########.fr       */
+/*   Updated: 2023/05/24 01:57:39 by titouan_ck       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,31 @@
 
 # include <iostream>
 # include <string>
+# include <climits>
+
+class contact
+{
+	public:
+		bool		init;
+		int			n;
+		std::string	firstname;
+		std::string	lastname;
+		std::string	nickname;
+		std::string	phone;
+};
+
+class phoneBook
+{
+	private:
+		contact	contacts[8];
+
+	public:
+		void	add(std::string, std::string, std::string, std::string);
+
+		int		oldest();
+		void	print();
+
+		phoneBook();
+};
 
 #endif
