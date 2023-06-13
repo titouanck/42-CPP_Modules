fc1e9ad640bcd8f15b0dd6927f809e042b8b587a
titouanck <chevrier.titouan@gmail.com>
2023-06-13 17:42:59 +0200

    CPP_08/ex01

---
+++ CPP_08/ex02/inc/MutantStack.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchevrie <tchevrie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/13 17:07:26 by tchevrie          #+#    #+#             */
+/*   Updated: 2023/06/13 17:10:39 by tchevrie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+	#define MUTANTSTACK_HPP
+
+	#include <iostream>
+	#include <vector>
+	#include <list>
+	#include <stack>
+	#include <algorithm>
+
+	class MutantStack
+	{
+
+	};
+
+#endif
