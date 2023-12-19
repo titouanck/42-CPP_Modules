2af55ed7b1dcd7bbea544f13b48884aec9c016ec
titouanck <chevrier.titouan@gmail.com>
2023-12-19 11:48:39 +0100

    ex00

--- CPP_06/ex00/inc/ScalarConverter.hpp
+++ CPP_06/ex00/inc/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: titouanck <chevrier.titouan@gmail.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 12:05:34 by titouanck         #+#    #+#             */
-/*   Updated: 2023/12/18 19:04:20 by titouanck        ###   ########.fr       */
+/*   Updated: 2023/12/19 11:48:01 by titouanck        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 	#include <iostream>
 	#include <iomanip>
 	#include <cfloat>
+	#include <cstdlib>
 
 	#define FLT_MY_MIN -FLT_MAX
 	#define DBL_MY_MIN -DBL_MAX
